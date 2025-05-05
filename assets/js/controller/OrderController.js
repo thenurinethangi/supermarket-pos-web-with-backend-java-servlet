0b490e3daedf2808ebf0ae2e9336fe1fb3b461e5
thenurinethangi <thenurinathangi@gmail.com>
2025-05-05 14:20:32 +0530

    updated code

--- assets/js/controller/OrderController.js
+++ assets/js/controller/OrderController.js
@@ -46,6 +46,61 @@ function loadOrderTable() {
 
 
 
+//view order deatils icon
+var selectedCustomerIdTodelete = null;
+
+$(document).on('click', '.view-order-icon', function () {
+
+    let parentRow = $(this).closest('tr');
+    let childrens = parentRow[0].childNodes;
+    console.log(childrens);
+
+    let orderDetail = "Order ID: "+childrens[1].innerHTML+"\n"+
+        "Customer ID: "+childrens[3].innerHTML+"\n"+
+        "Date: "+childrens[5].innerHTML+"\n"+
+        "Item Count: "+childrens[7].innerHTML+"\n"+
+        "Total: "+childrens[9].innerHTML;
+
+    let orderDetailsTextArea = $('#order-details')[0];
+    orderDetailsTextArea.value = orderDetail;
+
+    let order = null;
+
+    for (let i = 0; i <orderDB.length ; i++) {
+        let id = orderDB[i].orderId;
+
+        if(childrens[1].innerHTML==id){
+            order = orderDB[i];
+            break;
+        }
+    }
+
+    let itemDeatils = "";
+
+    if(order!=null){
+
+        for (let i = 0; i < order.itemList.length; i++) {
+            let itemId = order.itemList[i];
+            console.log(itemId);
+
+           for (let j = 0; j < itemDB.length; j++) {
+                let iId = itemDB[j].id;
+
+                if(iId==itemId){
+                    itemDeatils +="Item ID: "+itemDB[i].id+", Description: "+itemDB[i].description+"\n";
+                    break;
+                }
+            }
+        }
+    }
+
+    let itemDetailsTextArea = $('#item-details')[0];
+    itemDetailsTextArea.value = itemDeatils;
+});
+
+
+
+
 
 
 loadOrderTable();
