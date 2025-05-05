8f2751776e2f8ac2aa8a32984ce1585e3afaae33
thenurinethangi <thenurinathangi@gmail.com>
2025-05-05 21:45:10 +0530

    updated code

---
+++ assets/js/model/CartModel.js
@@ -0,0 +1,9 @@
+class CartModel{
+
+    constructor(itemId,price,qty,total) {
+        this.itemId = itemId;
+        this.price = price;
+        this.qty = qty;
+        this.total = total;
+    }
+}
