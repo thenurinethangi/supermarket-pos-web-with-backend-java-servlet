c146663f4766452e77135ac5d0904d15f5caabbe
thenurinethangi <thenurinathangi@gmail.com>
2025-06-08 22:37:55 +0530

    place order repo develop

--- BackEnd/src/main/java/org/example/repository/PlaceOrderRepository.java
+++ BackEnd/src/main/java/org/example/repository/PlaceOrderRepository.java
@@ -1,4 +1,35 @@
 package org.example.repository;
 
+import org.example.config.FactoryConfiguration;
+import org.example.entity.Orders;
+import org.hibernate.Session;
+
 public class PlaceOrderRepository {
+
+    public String generateNewId() {
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+
+        try{
+            Orders orders = session.createQuery("from Orders order by id desc",Orders.class).setMaxResults(1).uniqueResult();
+
+            if(orders==null){
+                return "ORD-000001";
+            }
+
+            String lastId = orders.getId();
+            String noPart = lastId.substring(4);
+            int no = Integer.parseInt(noPart);
+            no++;
+            String newId = String.format("ORD-%06d",no);
+            return newId;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ORD-000001";
+        }
+        finally {
+            session.close();
+        }
+    }
 }
