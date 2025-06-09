2fb7a710406cbd4df0c0e46020fb199ce63544b8
thenurinethangi <thenurinathangi@gmail.com>
2025-06-09 11:37:49 +0530

    add place order controller to doPost method

--- BackEnd/src/main/java/org/example/controller/PlaceOrderController.java
+++ BackEnd/src/main/java/org/example/controller/PlaceOrderController.java
@@ -6,12 +6,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.dto.OrderDto;
 import org.example.entity.Customer;
 import org.example.entity.Item;
+import org.example.entity.Orders;
 import org.example.service.PlaceOrderService;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @WebServlet("/placeorder/*")
 public class PlaceOrderController extends HttpServlet {
@@ -56,6 +59,44 @@ public class PlaceOrderController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,String> order = objectMapper.readValue(req.getInputStream(), Map.class);
+
+        Object itemListObj = order.get("itemList");
+        String[] ar;
+
+        if (itemListObj instanceof ArrayList) {
+            ArrayList<String> itemList = (ArrayList<String>) itemListObj;
+            ar = itemList.toArray(new String[0]);
+        } else if (itemListObj instanceof String) {
+            String itemList = (String) itemListObj;
+            ar = itemList.split(",");
+        } else {
+            throw new IllegalArgumentException("Invalid itemList format");
+        }
+        List<Item> items = new ArrayList<>();
+        for (int i = 0; i < ar.length; i=i+2) {
+            Item item = placeOrderService.searchItemById(ar[i]);
+            items.add(item);
+        }
+
+        Customer customer = placeOrderService.searchCustomerById(order.get("customer"));
+
+        Orders orders = new Orders(order.get("id"),order.get("date"),Double.parseDouble(order.get("total")),Integer.parseInt(order.get("itemCount")),customer,items);
+
+        boolean result = placeOrderService.placeOrder(orders,ar);
+
+        if(result) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write("true");
+        }
+        else{
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().write("false");
+        }
 
     }
 }
